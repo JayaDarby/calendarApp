9f13d69519c5422c3968f6cec78c4f366417b0f9
JayaDarby <jayadarby@gmail.com>
2015-06-24 15:49:30 -0700

    adding user auth

--- models/user.js
+++ models/user.js
@@ -15,5 +15,64 @@ var userSchema = new mongoose.Schema({
 	},
 });
 
+
+
+userSchema.pre('save', function(next){
+
+  var user = this;
+  if(!user.isModified('password')){
+  	return next();
+  }
+
+  return bcrypt.genSalt(SALT_FACTOR, function(err, salt){
+	if(err){
+		return next(err);
+	}
+	return bcrypt.hash(user.password, salt, function(err, hash) {
+		if(err){
+			return next(err);
+		}
+		user.password = hash;
+
+		return next();
+	});
+  });
+});
+
+
+
+
+userSchema.statics.authenticate = function (formData, callback) {
+  // this refers to the model!
+  this.findOne({
+      email: formData.email
+    },
+    function (err, user) {
+      if (user === null){
+        callback("Invalid username or password", null);
+      }
+      else {
+        user.checkPassword(formData.password, callback);
+      }
+
+    });
+};
+
+
+
+userSchema.methods.checkPassword = function(password, callback) {
+  var user = this;
+  bcrypt.compare(password, user.password, function (err, isMatch) {
+    if (isMatch) {
+      callback(null, user);
+    } else {
+      callback(err, null);
+    }
+  });
+};
+
+
+
 var User = mongoose.model('User', userSchema);
 
+module.exports = User;
